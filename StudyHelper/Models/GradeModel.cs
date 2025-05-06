3da32ee6bc42a7c6293468bf01ac8b8970098eac
Mariia Tkachova <115027762+mashatkk@users.noreply.github.com>
2025-05-06 10:20:44 +0300

    Update GradeModel.cs

--- StudyHelper/Models/GradeModel.cs
+++ StudyHelper/Models/GradeModel.cs
@@ -1,4 +1,4 @@
-using System;
+/*using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -23,5 +23,39 @@ namespace StudyApp.Models
         // Navigation property
         [ForeignKey("UserId")]
         public virtual User? User { get; set; }
+    }
+}
+*/
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudyApp.Models
+{
+    public class GradeModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Предмет є обов'язковим.")]
+        [Display(Name = "Предмет")]
+        public string Subject { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Оцінка є обов'язковою.")]
+        [Display(Name = "Оцінка")]
+        public double Grade { get; set; }
+
+        [Required(ErrorMessage = "Дата є обов'язковою.")]
+        [Display(Name = "Дата")]
+        public DateTime Date { get; set; } = DateTime.Today;
+
+        [Display(Name = "Ідентифікатор користувача")]
+        public string? UserId { get; set; }
+
+        // Navigation property
+        [ForeignKey("UserId")]
+        [Display(Name = "Користувач")]
+        public virtual User? User { get; set; }
+ 
     }
 }
