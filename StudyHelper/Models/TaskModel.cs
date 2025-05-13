b41fb01cbaa9056d77b5be5a0801199b4e76700c
Mariia Tkachova <115027762+mashatkk@users.noreply.github.com>
2025-05-13 09:46:44 +0300

    Update TaskModel.cs

--- StudyHelper/Models/TaskModel.cs
+++ StudyHelper/Models/TaskModel.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel.DataAnnotations;
+/*using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StudyApp.Models
@@ -23,5 +23,38 @@ namespace StudyApp.Models
 
         [ForeignKey("UserId")]
         public string Author { get; set; }  
+    }
+}*/
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudyApp.Models
+{
+    public class TaskModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Назва завдання є обов'язковою.")]
+        [Display(Name = "Назва")]
+        public string Title { get; set; } = string.Empty;
+
+        [Display(Name = "Опис")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Термін виконання є обов'язковим.")]
+        [Display(Name = "Дедлайн")]
+        public DateTime Deadline { get; set; }
+
+        [Display(Name = "Завершено")]
+        public bool IsCompleted { get; set; } = false;
+
+        [Required]
+        [Display(Name = "Ідентифікатор користувача")]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        [Display(Name = "Автор")]
+        public string Author { get; set; }
     }
 }
